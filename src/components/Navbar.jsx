51106973b89c4899dcb4eb1bf7c6f3d4cbacb380
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-03-11 17:58:23 +0100

    last commit of the day

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import ironhackLogo from "../assets/ironhack-logo.png"
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <img className="logo" src={ironhackLogo} alt=""></img>
+        <h1>Ironhack BNB</h1>
+    </nav>
+    
+  )
+}
---
+++ src/page/Homepage.jsx
@@ -0,0 +1,15 @@
+import { Footer } from "../components/Footer"
+import { Navbar } from "../components/Navbar"
+import { Sidebar } from "../components/Sidebar"
+
+export const Homepage = () => {
+  return (
+    <>
+     <Navbar />
+      <Sidebar />
+     <Footer />
+
+    </>
+  )
+}
+
