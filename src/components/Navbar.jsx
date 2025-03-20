f70534a825ef106eff75280400f1de9d577bde21
CannyRo <whatcannydev@gmail.com>
2025-03-20 01:47:08 +0100

    Style of Create property page from 375 to 1440px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import logo from "../assets/logo-rental-hack.svg";
 import logout from "../assets/arrow-right-from-bracket-icon.svg";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <>
       <nav className="navbar">
         <div className="logo-container">
-          <div className="logo-background">
-            <img src={logo} alt="Iron Rental" className="logo" />
-          </div>
+          <Link to="/">
+            <div className="logo-background">
+              <img src={logo} alt="Iron Rental" className="logo" />
+            </div>
+          </Link>
         </div>
         <hr className="divider-navbar" />
         <div className="title-container">
